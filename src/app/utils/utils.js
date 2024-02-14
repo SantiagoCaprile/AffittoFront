17370b157481e55856ccfab48934aaa6761495a5
Santi Caprile <santiagocaprile7@gmail.com>
2024-02-14 19:50:38 -0300

    Agregué la funcion de mostrarMontoSeparado a Utils

--- src/app/utils/utils.js
+++ src/app/utils/utils.js
@@ -1,3 +1,13 @@
 export function fixedDate(date) {
 	return date.split("T")[0].split("-").reverse().join("/");
 }
+
+export function mostrarMontoSeparado(monto) {
+	if (!monto) return "0";
+	const montoString = monto.toString();
+	const [parteEntera, parteDecimal] = montoString.split(".");
+	const parteEnteraSeparada = parteEntera.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+	return parteDecimal
+		? `${parteEnteraSeparada},${parteDecimal}`
+		: parteEnteraSeparada;
+}
