6fda4aef52fe9e71e4a9cf92a636666e57502544
Santi Caprile <santiagocaprile7@gmail.com>
2024-02-18 08:29:02 -0300

    Cambié la ruta de la página de información de contrato y le hice mejoras

--- src/app/propiedades/[id]/[idContrato]/page.jsx
+++ src/app/propiedades/[id]/[idContrato]/page.jsx
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import Contrato from "@/classes/Contrato";
+import { fixedDate } from "@/app/utils/utils";
 import Link from "next/link";
 import {
 	Pencil,
@@ -13,63 +14,16 @@ import {
 } from "lucide-react";
 import { mostrarMontoSeparado } from "@/app/utils/utils";
 
-const contratoEjemplo = {
-	_id: "1234567890",
-	destino: "Vivienda",
-	fecha_inicio: "2021-10-01",
-	fecha_fin: "2022-10-01",
-	monto: 20000,
-	moneda: "ARS",
-	comision_celebracion: "200 ARS",
-	observaciones:
-		"Solo se aceptan pagos en efectivo, no se aceptan mascotas, no se aceptan niños. Solo se aceptan pagos en efectivo, no se aceptan mascotas, no se aceptan niños. Solo se aceptan pagos en efectivo, no se aceptan mascotas, no se aceptan niños.",
-	estado: "Vigente", //Vigente, Caducado, Rescindido
-	fecha_creacion: "2021-10-01",
-	fecha_modificacion: "2021-10-01",
-	propiedad: {
-		_id: "1234567890",
-		domicilio: "Calle Falsa 123",
-		localidad: "Rosario",
-		tipo_propiedad: "Casa",
-		superficie: 100,
-		descripcion: "Descripcion",
-		precio: 20000,
-		estado_actual: "Disponible",
-	},
-	locatario: {
-		_id: "1234567890",
-		nombre_razon_social: "Esteban Quito",
-		cuit: "20-12345678-9",
-		celular: "1234567890",
-		email: "example@gmail.com",
-	},
-	garantes: [
-		{
-			_id: "1234567890",
-			nombre_razon_social: "Tuki Tuki",
-			cuit: "16415555551",
-			celular: "1234567890",
-			email: "example@gmail.com",
-		},
-	],
-	propietario: {
-		_id: "1234567890",
-		nombre_razon_social: "Tucson",
-		cuit: "20-12345678-9",
-		celular: "1234567890",
-		email: "example@gmail.com",
-	},
-};
-
 export default function InfoContratoPage() {
 	const router = useRouter();
 	const [contrato, setContrato] = useState({});
 
 	useEffect(() => {
-		const _id = document.location.pathname.split("/")[2];
+		const _id = document.location.pathname.split("/")[3];
 		async function fetchData() {
-			//const contrato = await Contrato.buscarContrato(_id);
-			setContrato(contratoEjemplo);
+			const contrato = await Contrato.buscarContrato(_id);
+			console.log(contrato);
+			setContrato(contrato);
 		}
 		fetchData();
 	}, []);
@@ -88,6 +42,10 @@ export default function InfoContratoPage() {
 		}
 	};
 
+	if (!contrato?._id) {
+		return <div>Cargando...</div>;
+	}
+
 	return (
 		<div className="flex flex-1 justify-center items-center bg-[#E8EFFF] pt-4 gap-2">
 			<div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 max-w-[1200px] w-2/3 flex flex-col gap-4">
@@ -95,7 +53,7 @@ export default function InfoContratoPage() {
 					<h2 className="text-2xl font-bold mb-4">Información de Contrato</h2>
 					<div className="flex justify-end gap-2">
 						<Link
-							href={"/contratos/editar"}
+							href={`/propiedades/${contrato.propiedad?._id}/${contrato._id}/editar`}
 							className="flex gap-2 bg-blue-500 hover:bg-blue-600 transition-all text-white px-4 py-2 rounded-md items-center justify-center"
 						>
 							<Pencil size={20} />
@@ -111,27 +69,23 @@ export default function InfoContratoPage() {
 					</div>
 				</div>
 				<div className="flex justify-around mb-2">
-					<div>
-						<span className="font-bold">Domicilio: </span>
-						<span>{contrato.propiedad?.domicilio}</span>
-					</div>
-					<div>
-						<span className="font-bold">Localidad: </span>
-						<span>{contrato.propiedad?.localidad}</span>
-					</div>
-					<div>
-						<span className="font-bold">Tipo de Propiedad: </span>
-						<span>{contrato.propiedad?.tipo_propiedad}</span>
-					</div>
+					{Object.entries(contrato.propiedad?.domicilio).map(([key, value]) => (
+						<div key={key}>
+							<span className="font-bold">
+								{key[0].toUpperCase() + key.slice(1)}:{" "}
+							</span>
+							<span>{value}</span>
+						</div>
+					))}
 				</div>
 				<table className="w-full text-m text-left text-gray-500 dark:text-gray-400">
 					<thead className="text-m text-gray-700 p-1 uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
 						<tr className=" bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
-							<th className={styles.th}>Nombre/Razon Social</th>
+							<th className={styles.th}>Nombre/Razón Social</th>
 							<th className={styles.th}>CUIT/CUIL</th>
 							<th className={styles.th}>Celular</th>
 							<th className={styles.th}>E-Mail</th>
-							<th className={styles.th}>Condicion</th>{" "}
+							<th className={styles.th}>Condición</th>
 						</tr>
 					</thead>
 					<tbody className="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
@@ -152,20 +106,28 @@ export default function InfoContratoPage() {
 								<td className={styles.td}>Locatario</td>
 							</tr>
 						)}
-						{contrato.propietario && (
+						{contrato.propiedad.propietario && (
 							<tr
-								key={contrato.propietario._id}
+								key={contrato.propiedad.propietario._id}
 								className={styles.tr}
 								onDoubleClick={() =>
-									router.push(`/clientes/${contrato.propietario.cuit}`)
+									router.push(
+										`/clientes/${contrato.propiedad.propietario.cuit}`
+									)
 								}
 							>
 								<td className={styles.td}>
-									{contrato.propietario.nombre_razon_social}
+									{contrato.propiedad.propietario.nombre_razon_social}
 								</td>
-								<td className={styles.td}>{contrato.propietario.cuit}</td>
-								<td className={styles.td}>{contrato.propietario.celular}</td>
-								<td className={styles.td}>{contrato.propietario.email}</td>
+								<td className={styles.td}>
+									{contrato.propiedad.propietario.cuit}
+								</td>
+								<td className={styles.td}>
+									{contrato.propiedad.propietario.celular}
+								</td>
+								<td className={styles.td}>
+									{contrato.propiedad.propietario.email}
+								</td>
 								<td className={styles.td}>Propietario</td>
 							</tr>
 						)}
@@ -217,25 +179,33 @@ export default function InfoContratoPage() {
 							</span>
 						)}
 					</div>
-					<div className="mb-2">
+					<div className="mb-2 grid">
 						<span className="font-bold">Fecha Inicio: </span>
-						<span>{contrato.fecha_inicio}</span>
+						<span>{fixedDate(contrato.fecha_inicio)}</span>
 					</div>
-					<div className="mb-2">
+					<div className="mb-2 grid">
 						<span className="font-bold">Fecha Fin: </span>
-						<span>{contrato.fecha_fin}</span>
+						<span>{fixedDate(contrato.fecha_fin)}</span>
 					</div>
 					<div className="mb-2">
-						<span className="font-bold">Comision Celebracion: </span>
+						<span className="font-bold">Comision Celebración: </span>
 						<span>{contrato.comision_celebracion}</span>
 					</div>
-					<div className="mb-2">
-						<span className="font-bold">Fecha de Creacion: </span>
-						<span>{contrato.fecha_creacion}</span>
+					<div className="mb-2 grid">
+						<span className="font-bold">Fecha de Creación: </span>
+						<span>
+							{fixedDate(contrato.createdAt) +
+								" - " +
+								contrato.createdAt.split("T")[1].split(".")[0]}
+						</span>
 					</div>
-					<div className="mb-2">
-						<span className="font-bold">Fecha de Modificacion: </span>
-						<span>{contrato.fecha_modificacion}</span>
+					<div className="mb-2 grid">
+						<span className="font-bold">Fecha de Modificación: </span>
+						<span>
+							{fixedDate(contrato.updatedAt) +
+								" - " +
+								contrato.updatedAt.split("T")[1].split(".")[0]}
+						</span>
 					</div>
 				</div>
 				<div className="mb-2">
